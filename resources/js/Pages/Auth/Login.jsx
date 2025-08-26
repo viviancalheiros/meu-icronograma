5bddfc16d3255586c5aa2f57844f3d362b514841
viviancalheiros <viviancalheiros17@hotmail.com>
2025-08-26 11:50:06 -0300

    update logos

--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -1,12 +1,20 @@
 import { Link } from '@inertiajs/react';
 import Title from '@/Components/Title';
 import Button from '@/Components/Button';
+import LogoUfal from '@/Components/LogoUfal';
+import LogoIc from '@/Components/LogoIC';
 
 export default function Login() {
     return (
        <div className='w-screen h-screen flex font-roboto'>
-        <div className='bg-purple-dark w-1/2 flex flex-col items-center justify-center'>
-            <Title />
+        <div className='bg-purple-dark w-1/2 flex flex-col items-center'>
+            <div className='w-full h-3/6 flex flex-row pt-4 justify-center'>
+                <LogoUfal width={35} height={76} className="ml-8" />
+                <LogoIc width={80} height={80} className="ml-4" />
+            </div>
+            <div className='w-full h-4/6 flex flex-row'>
+                <Title />
+            </div>
         </div>
         <div className='bg-white w-1/2 flex flex-col items-center justify-center'>
             <div className='bg-purple-dark w-3/6 flex flex-col items-center rounded-2xl shadow-2xl'>
@@ -17,7 +25,7 @@ export default function Login() {
                     <input type='text' placeholder='Senha' required 
                         className='bg-white h-12 pl-4 rounded-2xl text-sm w-full placeholder:text-purple-dark'></input>
                     <Link href="" className='text-white self-start mt-4 mb-12'>Esqueceu a senha?</Link>
-                    <Button value={'ENVIAR'} />
+                    <Button value={'ENTRAR'} />
                 </form>
                 <Link href="" className='text-white mb-8'>Cadastre-se</Link>
             </div>
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <title inertia>{{ config('app.name', 'Meu ICronograma') }}</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
@@ -12,7 +12,6 @@
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
-
 
         <!-- Scripts -->
         @routes
