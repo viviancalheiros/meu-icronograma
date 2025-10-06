bba63b2180619c38e4ed1cfa3bd89d12ea9598ab
e <jwfd@ic.ufal.br>
2025-10-05 23:48:23 -0300

    migrate tabela diciplinas e usuarios

---
+++ database/migrations/2025_10_06_005632_create_disciplinas_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('disciplinas', function (Blueprint $table) {
+            $table->id();
+            $table->string("nome");
+            $table->integer("horas");
+            $table->boolean('tipo_diciplina');
+            $table->integer("periodo")->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('disciplinas');
+    }
+};
