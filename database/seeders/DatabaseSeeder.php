51108582895322c65d4816877388fed2ed66d9b8
e <jwfd@ic.ufal.br>
2025-10-07 19:14:54 -0300

    Criando as outras migrations e algumas seeds

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Test;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,11 +14,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        $this->call(DisciplinaSeeder::class);
+        $this->call(EnfasesSeeder::class);
+        $this->call(Tipos_horas_complementaresSeeder::class);
     }
 }
---
+++ database/seeders/DisciplinaSeeder.php
@@ -0,0 +1,216 @@
+<?php
+
+namespace Database\Seeders;
+
+use Carbon\Carbon;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class DisciplinaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $disciplinas = [
+            [
+                "nome" => "Programação 1",
+                "horas" => 72,
+                "tipo_diciplina" => true,
+                "periodo" => 1,
+                "updated_at" => Carbon::now(),
+                "created_at" => Carbon::now()
+            ],
+            [
+                "nome" => "Lógica para Computação",
+                "horas" => 72,
+                "tipo_diciplina" => true,
+                "periodo" => 1,
+                "updated_at" => Carbon::now(),
+                "created_at" => Carbon::now()
+            ],
+            [
+                "nome" => "Computação, Sociedade e Ética",
+                "horas" => 72,
+                "tipo_diciplina" => true,
+                "periodo" => 1,
+                "updated_at" => Carbon::now(),
+                "created_at" => Carbon::now()
+            ],
+            [
+                "nome" => "Matemática Discreta",
+                "horas" => 144,
+                "tipo_diciplina" => true,
+                "periodo" => 1,
+                "updated_at" => Carbon::now(),
+                "created_at" => Carbon::now()
+            ],
+            [
+                "nome" => "Cálculo Diferencial e Integral",
+                "horas" => 72,
+                "tipo_diciplina" => true,
+                "periodo" => 1,
+                "updated_at" => Carbon::now(),
+                "created_at" => Carbon::now()
+            ],
+            [
+                "nome" => "Estrutura de Dados",
+                "horas" => 72,
+                "tipo_diciplina" => true,
+                "periodo" => 2,
+                "updated_at" => Carbon::now(),
+                "created_at" => Carbon::now()
+            ],
+            [
+                "nome" => "Banco de Dados",
+                "horas" => 72,
+                "tipo_diciplina" => true,
+                "periodo" => 2,
+                "updated_at" => Carbon::now(),
+                "created_at" => Carbon::now()
+            ],
+            [
+                "nome" => "Organização e Arquitetura de Computadores",
+                "horas" => 72,
+                "tipo_diciplina" => true,
+                "periodo" => 2,
+                "updated_at" => Carbon::now(),
+                "created_at" => Carbon::now()
+            ],
+            [
+                "nome" => "Geometria Analítica",
+                "horas" => 72,
+                "tipo_diciplina" => true,
+                "periodo" => 2,
+                "updated_at" => Carbon::now(),
+                "created_at" => Carbon::now()
+            ],
+            [
+                "nome" => "Redes de Computadores",
+                "horas" => 72,
+                "tipo_diciplina" => true,
+                "periodo" => 3,
+                "updated_at" => Carbon::now(),
+                "created_at" => Carbon::now()
+            ],
+            [
+                "nome" => "Teoria dos Grafos",
+                "horas" => 72,
+                "tipo_diciplina" => true,
+                "periodo" => 3,
+                "updated_at" => Carbon::now(),
+                "created_at" => Carbon::now()
+            ],
+            [
+                "nome" => "Probabilidade e Estatística",
+                "horas" => 72,
+                "tipo_diciplina" => true,
+                "periodo" => 3,
+                "updated_at" => Carbon::now(),
+                "created_at" => Carbon::now()
+            ],
+            [
+                "nome" => "Álgebra Linear",
+                "horas" => 72,
+                "tipo_diciplina" => true,
+                "periodo" => 3,
+                "updated_at" => Carbon::now(),
+                "created_at" => Carbon::now()
+            ],
+            [
+                "nome" => "Programação 2",
+                "horas" => 72,
+                "tipo_diciplina" => true,
+                "periodo" => 4,
+                "updated_at" => Carbon::now(),
+                "created_at" => Carbon::now()
+            ],
+            [
+                "nome" => "Programação 3",
+                "horas" => 72,
+                "tipo_diciplina" => true,
+                "periodo" => 4,
+                "updated_at" => Carbon::now(),
+                "created_at" => Carbon::now()
+            ],
+            [
+                "nome" => "Projeto e Análise de Algoritmos",
+                "horas" => 72,
+                "tipo_diciplina" => true,
+                "periodo" => 4,
+                "updated_at" => Carbon::now(),
+                "created_at" => Carbon::now()
+            ],
+            [
+                "nome" => "Teoria da Computação",
+                "horas" => 72,
+                "tipo_diciplina" => true,
+                "periodo" => 4,
+                "updated_at" => Carbon::now(),
+                "created_at" => Carbon::now()
+            ],
+            [
+                "nome" => "Sistemas Operacionais",
+                "horas" => 72,
+                "tipo_diciplina" => true,
+                "periodo" => 5,
+                "updated_at" => Carbon::now(),
+                "created_at" => Carbon::now()
+            ],
+            [
+                "nome" => "Compiladores",
+                "horas" => 72,
+                "tipo_diciplina" => true,
+                "periodo" => 5,
+                "updated_at" => Carbon::now(),
+                "created_at" => Carbon::now()
+            ],
+            [
+                "nome" => "Inteligência Artificial",
+                "horas" => 72,
+                "tipo_diciplina" => true,
+                "periodo" => 5,
+                "updated_at" => Carbon::now(),
+                "created_at" => Carbon::now()
+            ],
+            [
+                "nome" => "Computação Gráfica",
+                "horas" => 72,
+                "tipo_diciplina" => true,
+                "periodo" => 5,
+                "updated_at" => Carbon::now(),
+                "created_at" => Carbon::now()
+            ],
+            [
+                "nome" => "Projeto e Desenvolvimento de Sistemas",
+                "horas" => 288,
+                "tipo_diciplina" => true,
+                "periodo" => 6,
+                "updated_at" => Carbon::now(),
+                "created_at" => Carbon::now()
+            ],
+            [
+                "nome" => "Metodologia de Pesquisa e Trabalho Individual",
+                "horas" => 72,
+                "tipo_diciplina" => true,
+                "periodo" => 7,
+                "updated_at" => Carbon::now(),
+                "created_at" => Carbon::now()
+            ],
+            [
+                "nome" => "Noções de Direito",
+                "horas" => 72,
+                "tipo_diciplina" => true,
+                "periodo" => 7,
+                "updated_at" => Carbon::now(),
+                "created_at" => Carbon::now()
+            ]   
+            
+        ];
+
+        DB::table("disciplinas")->insert($disciplinas);
+    }
+}
