871f1fb639b4a12c7c058798cca940514b85e64c
viviancalheiros <viviancalheiros17@hotmail.com>
2025-11-02 23:37:55 -0300

    fix type periodo_pago

--- database/migrations/2025_10_15_000936_fix_disciplina_usuario_table_structure.php
+++ database/migrations/2025_10_15_000936_fix_disciplina_usuario_table_structure.php
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('disciplina_usuario', function (Blueprint $table) {
-            $table->integer('periodo_pago')->nullable()->change();
+            $table->string('periodo_pago')->nullable()->change();
         });
     }
 };
